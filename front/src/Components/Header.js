be2b016d62d7ebe932ec92178be270ce5aa6bdd4
Md. Fahmidul Karim Rafi <137741434+PDOX103@users.noreply.github.com>
2024-06-27 11:54:55 +0600

    commit

--- front/src/Components/Header.js
+++ front/src/Components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Logos from './Logos'
 
 const Header = () => {
   return (
-    <div>
-     head
-    </div>
+    <header className='h-16 shadow-md'>
+      <div className='container mx auto'>
+        <div className=''>
+          <Logos w={100} h={60}/>
+        </div>
+      </div>
+    </header>
   )
 }
 
