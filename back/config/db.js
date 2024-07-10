55976fa9c7ba17aacabc2893b3e1aeca2dc927e5
Md. Fahmidul Karim Rafi <137741434+PDOX103@users.noreply.github.com>
2024-07-11 05:40:23 +0600

    commit

---
+++ back/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+async function connectDB(){
+    try{
+        await mongoose.connect(process.env.MONGOOSE_URI)
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+module.exports = connectDB
