d8f6874ae4b98786bf0445363b3155e5735f485f
Md. Fahmidul Karim Rafi <137741434+PDOX103@users.noreply.github.com>
2024-09-19 12:56:57 +0600

    Place orders completed

--- front/src/pages/Orders.jsx
+++ front/src/pages/Orders.jsx
@@ -15,7 +15,20 @@ const Orders = () => {
         return null
       }
       const response = await axios.post(backendUrl + '/api/order/userorders', {},{headers:{token}})
-      console.log(response.data);
+      if(response.data.success){
+        let allOrdersItem = []
+        response.data.orders.map((order)=>{
+          order.items.map((item)=>{
+            item['status'] = order.status
+            item['payment'] = order.payment
+            item['paymentMethod'] = order.paymentMethod
+            item['date'] = order.date
+
+            allOrdersItem.push(item)
+          })
+        })
+        setOrderData(allOrdersItem.reverse())
+      }
     }catch(error){
 
     }
