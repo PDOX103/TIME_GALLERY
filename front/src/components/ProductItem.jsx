96b108cca2200c33cf8ffaf47ef6fcb1a2c2da61
Md. Fahmidul Karim Rafi <137741434+PDOX103@users.noreply.github.com>
2024-09-22 02:31:59 +0600

    Update ProductItem.jsx

--- front/src/components/ProductItem.jsx
+++ front/src/components/ProductItem.jsx
@@ -1,18 +1,32 @@
-import React, { useContext } from 'react'
-import { ShopContext } from '../context/ShopContext'
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import { Link } from 'react-router-dom';
 
-const ProductItem = ({id, image, name, price}) => {
-    const {currency} =useContext(ShopContext)
+const ProductItem = ({ id, image, name, price }) => {
+  const { currency } = useContext(ShopContext);
+
   return (
-    <Link className='text-gray-700 cursor-pointer' to = {`/product/${id}`}>
-        <div className='overflow-hidden'>
-            <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt=""/>
+    <Link className="text-gray-900 cursor-pointer" to={`/product/${id}`}>
+      <div className="border-separate bg-green-400 border-black p-4 rounded-s-3xl overflow-hidden transform hover:translate-y-[-5px] hover:shadow-2xl transition-all duration-300 ease-in-out">
+        {/* Image */}
+        <div className="overflow-hidden">
+          <img
+            className="hover:scale-110 transition-transform duration-300 ease-in-out w-full h-auto rounded-lg"
+            src={image[0]}
+            alt={name}
+          />
         </div>
-        <p className='pt-3 pb-1 text-sm'>{name}</p>
-        <p className='text-sm font-medium'>{currency}{price}</p>
-    </Link>
-  )
-}
 
-export default ProductItem
+        {/* Product Name */}
+        <p className="pt-3 pb-1 text-sm font-medium truncate">{name}</p>
+
+        {/* Price */}
+        <p className=" text-sm font-bold text-red-900">
+          {currency}{price}
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductItem;
