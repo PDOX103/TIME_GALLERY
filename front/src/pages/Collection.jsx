2cc10cdab8c1d53c38ce672ad2e42bc73ad6546b
Md. Fahmidul Karim Rafi <137741434+PDOX103@users.noreply.github.com>
2024-09-22 04:08:00 +0600

    Update front

--- front/src/pages/Collection.jsx
+++ front/src/pages/Collection.jsx
@@ -12,137 +12,193 @@ const Collection = () => {
   const [subCategory, setSubCategory] = useState([]);
   const [sortType, setSortType] = useState('relavent');
 
-
-  const toggleCategory = (e)=>{
-    if(category.includes(e.target.value)){
-      setCategory(prev=> prev.filter(item => item!==e.target.value))
+  const toggleCategory = (e) => {
+    if (category.includes(e.target.value)) {
+      setCategory((prev) => prev.filter((item) => item !== e.target.value));
+    } else {
+      setCategory((prev) => [...prev, e.target.value]);
     }
-    else{
-      setCategory(prev=>[...prev, e.target.value])
-    }
-  }
+  };
 
-
-  const toggleSubCategory = (e)=>{
-    if(subCategory.includes(e.target.value)){
-      setCategory(prev=> prev.filter(item => item!==e.target.value))
+  const toggleSubCategory = (e) => {
+    if (subCategory.includes(e.target.value)) {
+      setSubCategory((prev) => prev.filter((item) => item !== e.target.value));
+    } else {
+      setSubCategory((prev) => [...prev, e.target.value]);
     }
-    else{
-      setSubCategory(prev=>[...prev, e.target.value])
-    }
-  }
+  };
 
-  const applyFilter = () =>{
+  const applyFilter = () => {
     let productsCopy = products.slice();
 
-    if(showSearch && search){
-      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter((item) =>
+        item.name.toLowerCase().includes(search.toLowerCase())
+      );
     }
 
-    if(category.length >0){
-      productsCopy = productsCopy.filter(item => category.includes(item.category));
+    if (category.length > 0) {
+      productsCopy = productsCopy.filter((item) =>
+        category.includes(item.category)
+      );
     }
 
-    // if(subCategory.length >0){
-    //   productsCopy = productsCopy.filter(item => subCategory.includes(item.subCategory));
-    // }
+    if (subCategory.length > 0) {
+      productsCopy = productsCopy.filter((item) =>
+        subCategory.includes(item.subCategory)
+      );
+    }
 
-    setFilterProducts(productsCopy)
-  }
+    setFilterProducts(productsCopy);
+  };
 
-
-  const sortProduct =()=>{
+  const sortProduct = () => {
     let fpCopy = filterProducts.slice();
 
-    switch(sortType){
+    switch (sortType) {
       case 'low-high':
-        setFilterProducts(fpCopy.sort((a,b)=> (a.price - b.price)));
+        setFilterProducts(fpCopy.sort((a, b) => a.price - b.price));
         break;
 
       case 'high-low':
-        setFilterProducts(fpCopy.sort((a,b)=> (b.price - a.price)));
+        setFilterProducts(fpCopy.sort((a, b) => b.price - a.price));
         break;
 
       default:
         applyFilter();
-        break;  
+        break;
     }
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     applyFilter();
-  },[category, subCategory, search, showSearch, products])
+  }, [category, subCategory, search, showSearch, products]);
 
-
-  useEffect(()=>{
+  useEffect(() => {
     sortProduct();
-  },[sortType])
+  }, [sortType]);
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
       {/* Filter Options */}
       <div className='min-w-60'>
-        <p onClick={()=>setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS
-          <img className={`h-3 sm:hidden ${showFilter? 'rotate-90' : ''}`} src={assets.dropdown_icon} alt=""/>
+        <p
+          onClick={() => setShowFilter(!showFilter)}
+          className='my-2 text-xl flex items-center cursor-pointer gap-2'
+        >
+          FILTERS
+          <img
+            className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`}
+            src={assets.dropdown_icon}
+            alt=''
+          />
         </p>
 
         {/* Category Filter */}
-        <div className={`border-2 border-green-500 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
+        <div
+          className={`border-2 border-green-500 pl-5 py-3 mt-6 ${
+            showFilter ? '' : 'hidden'
+          } sm:block`}
+        >
           <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex gap-2'>
-              <input className='w-3' type='checkbox' value={'Men'} onChange={toggleCategory}/>Men
+              <input
+                className='w-3'
+                type='checkbox'
+                value={'Men'}
+                onChange={toggleCategory}
+              />
+              Men
             </p>
             <p className='flex gap-2'>
-              <input className='w-3' type='checkbox' value={'Women'} onChange={toggleCategory}/>Women
+              <input
+                className='w-3'
+                type='checkbox'
+                value={'Women'}
+                onChange={toggleCategory}
+              />
+              Women
             </p>
             <p className='flex gap-2'>
-              <input className='w-3' type='checkbox' value={'Kids'} onChange={toggleCategory}/>Kids
+              <input
+                className='w-3'
+                type='checkbox'
+                value={'Kids'}
+                onChange={toggleCategory}
+              />
+              Kids
             </p>
           </div>
         </div>
-        {/* SubCategory Filter */}
 
-        {/* 
-        <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
+        {/* SubCategory Filter */}
+        <div
+          className={`border-2 border-green-500 pl-5 py-3 mt-6 ${
+            showFilter ? '' : 'hidden'
+          } sm:block`}
+        >
           <p className='mb-3 text-sm font-medium'>TYPE</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex gap-2'>
-              <input className='w-3' type='checkbox' value={'Smart Watch'} onChange={toggleSubCategory}/>Smart Watch
+              <input
+                className='w-3'
+                type='checkbox'
+                value={'Smart Watch'}
+                onChange={toggleSubCategory}
+              />
+              Smart Watch
             </p>
             <p className='flex gap-2'>
-              <input className='w-3' type='checkbox' value={'Digital Watch'} onChange={toggleSubCategory}/>Digital Watch
+              <input
+                className='w-3'
+                type='checkbox'
+                value={'Digital Watch'}
+                onChange={toggleSubCategory}
+              />
+              Digital Watch
             </p>
             <p className='flex gap-2'>
-              <input className='w-3' type='checkbox' value={'Sports Watch'} onChange={toggleSubCategory}/>Sports Watch
+              <input
+                className='w-3'
+                type='checkbox'
+                value={'Sports Watch'}
+                onChange={toggleSubCategory}
+              />
+              Sports Watch
             </p>
           </div>
         </div>
-        */}
-
       </div>
 
       {/* Right Side */}
       <div className='flex-1'>
         <div className='flex justify-between text-base sm:text-2xl mb-4'>
-          <Title text1={'ALL'} text2={'COLLECTION'}/>
+          <Title text1={'ALL'} text2={'COLLECTION'} />
           {/*Product sort */}
-          <select onChange={(e)=>setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2'>
-            <option value = "relavent">Sort by: Relavent</option>
-            <option value = "low-high">Sort by: Low to High</option>
-            <option value = "high-low">Sort by: High to Low</option>
+          <select
+            onChange={(e) => setSortType(e.target.value)}
+            className='border-2 border-gray-300 text-sm px-2'
+          >
+            <option value='relavent'>Sort by: Relavent</option>
+            <option value='low-high'>Sort by: Low to High</option>
+            <option value='high-low'>Sort by: High to Low</option>
           </select>
         </div>
 
-        {/*Map Products*/}
+        {/* Map Products */}
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
-            {
-              filterProducts.map((item,index)=>(
-                <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
-              ))
-            }
+          {filterProducts.map((item, index) => (
+            <ProductItem
+              key={index}
+              id={item._id}
+              image={item.image}
+              name={item.name}
+              price={item.price}
+            />
+          ))}
         </div>
-
+          <br/>
       </div>
     </div>
   );
--- front/src/pages/Orders.jsx
+++ front/src/pages/Orders.jsx
@@ -48,26 +48,26 @@ const Orders = () => {
       <div>
         {
           orderData.map((item, index)=> (
-            <div key={index} className='py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4'>
+            <div key={index} className='py-4 border-t border-b text-gray-900 flex flex-col md:flex-row md:items-center md:justify-between gap-4'>
               <div  className='flex items-start gap-6 text-sm'>
-                <img className='w-16 sm:w-20' src={item.image[0]} alt=""/>
+                <img className='w-20 sm:w-20 border-2 border-red-700 rounded-lg' src={item.image[0]} alt=""/>
                 <div>
                   <p className='sm:text-base font-medium'>{item.name}</p>
-                  <div className='flex items-center gap-3 mt-2 text-base text-gray-700'>
-                    <p className='text-lg'>{currency}{item.price}</p>
+                  <div className='flex items-center gap-3 mt-2 text-base text-gray-900'>
+                    <p className='text-lg text-red-900 font-semibold'>{currency}{item.price}</p>
                     <p>Quantity: {item.quantity}</p>
-                    <p>Size: {item.size}</p>
+                    <p>Color: {item.size}</p>
                   </div>
-                  <p className='mt-2'>Date: <span className='text-gray-400'>{new Date(item.date).toDateString()}</span></p>
-                  <p className='mt-2'>Payment: <span className='text-gray-400'>{item.paymentMethod}</span></p>
+                  <p className='mt-2'>Date: <span className='text-gray-500'>{new Date(item.date).toDateString()}</span></p>
+                  <p className='mt-2'>Payment: <span className='text-gray-500'>{item.paymentMethod}</span></p>
               </div>
               </div>
               <div className='md:w-1/2 flex justify-between'>
                 <div className='flex items-center gap-2'>
-                  <p className='min-w-2 h-2 rounded-full bg-green-500'></p>
+                  <p className='min-w-2 h-2 rounded-full bg-green-600'></p>
                   <p className='text-sm md:text-base'>{item.status}</p>
                 </div>
-                <button onClick={loadOrderData} className='border px-4 text-sm font-medium rounded-sm'>Track Order</button>
+                <button onClick={loadOrderData} className='border-separate bg-green-500 px-4 py-1 text-sm font-medium rounded-lg'>Track Order</button>
               </div>
             </div>
           ))
