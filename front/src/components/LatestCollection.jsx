9dc2e497892bab048ec54247e78a166b211352a7
Md. Fahmidul Karim Rafi <137741434+PDOX103@users.noreply.github.com>
2024-09-21 13:33:38 +0600

    Update LatestCollection.jsx

--- front/src/components/LatestCollection.jsx
+++ front/src/components/LatestCollection.jsx
@@ -1,32 +1,35 @@
-import React, {useContext, useEffect, useState} from 'react'
-import { ShopContext } from '../context/ShopContext'
-import Title from './Title';
-import ProductItem from './ProductItem';
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const LatestCollection = () => {
+  const { products } = useContext(ShopContext);
+  const [latestProduct, setLatestProduct] = useState([]);
 
-    const {products} = useContext(ShopContext);
-    const [latestProduct, setLatestProduct] = useState([]);
-
-    useEffect(()=>{
-        setLatestProduct(products.reverse().slice(0,10));
-    },[products])
+  useEffect(() => {
+    const sortedProducts = [...products].reverse();
+    setLatestProduct(sortedProducts.slice(0, 10));
+  }, [products]);
 
   return (
-    <div className='my-10'>
-        <div className='text-center py-8 text-3xl'>
-            <Title text1={'LATEST'} text2={'COLLECTION'}/>
-        </div>
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
-            {
-                latestProduct.map((item, index)=>(
-                    <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
-                ))
-            }
-        </div>
-      
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LATEST"} text2={"COLLECTION"} />
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProduct.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LatestCollection
+export default LatestCollection;
